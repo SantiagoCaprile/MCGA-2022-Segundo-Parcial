6a4ee1fc6ce9eb373f62bd9f90631fcf34930821
Santiago Caprile <santiagocaprile7@gmail.com>
2022-11-11 14:57:45 -0300

    Se agregó el tachado de productos en vez de borrarlos de la lista, con el fin de poder restablecerlos

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -3,17 +3,29 @@ import styles from './productItem.module.css'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
-const ProductItem = ({product}) => {
-    const [show, setShow] = useState(true)
+const ProductItem = ({ product }) => {
+    const [deleted, setDeleted] = useState(false)
 
     const handleDelete = () => {
-        setShow(false)
+        setDeleted(!deleted)
         //delete product from db using thunk and id
     }
 
-    if(!show || product.isDeleted) {
-        return(
-            null
+    if (deleted || product.isDeleted) {
+        return (
+            <div className={[styles.item, styles.deleted].join(" ")}>
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+                <p>{product.stock} units</p>
+                <div className={styles.options}>
+                    <Link to={`/edit/${product._id}`} className={styles.buttons + " material-icons"}>
+                        edit
+                    </Link>
+                    <button onClick={handleDelete} className={styles.buttons + " material-icons"}>
+                        restart_alt
+                    </button>
+                </div>
+            </div>
         )
     }
     return (
@@ -22,8 +34,12 @@ const ProductItem = ({product}) => {
             <p>{product.price}</p>
             <p>{product.stock} units</p>
             <div className={styles.options}>
-                <Link to={`/edit/${product._id}`} className= {styles.buttons + " material-icons"}>edit</Link>
-                <button onClick={handleDelete} className={styles.buttons + " material-icons"}>delete</button>
+                <Link to={`/edit/${product._id}`} className={styles.buttons + " material-icons"}>
+                    edit
+                </Link>
+                <button onClick={handleDelete} className={styles.buttons + " material-icons"}>
+                    delete
+                </button>
             </div>
         </div>
     );
